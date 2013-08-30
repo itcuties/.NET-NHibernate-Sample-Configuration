f3c1395df4631ea9129451ed89422d89b80bc636
itcuties <team@itcuties.com>
2013-08-30 06:26:54 +0200

    Initial commit.

---
+++ ItCuties.NHibernate/ItCuties.NHibernate/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace ItCuties.NHibernate
+{
+    public class Customer
+    {
+        public virtual int CustomerId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+    }
+}
---
+++ ItCuties.NHibernate/ItCuties.NHibernate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using System.Reflection;
+
+namespace ItCuties.NHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "Server=.\\SqlExpress; Database=ItCutiesDemo; Integrated Security=SSPI;";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sessionFactory = cfg.BuildSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    var customers = session.CreateCriteria<Customer>()
+                        .List<Customer>();
+                    foreach (var customer in customers)
+                    {
+                        Console.WriteLine("{0} {1}", customer.FirstName, customer.LastName);
+                    }
+                    tx.Commit();
+                }
+                Console.WriteLine("Press <ENTER> to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
